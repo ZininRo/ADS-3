7bce9319a45102fb897118053e5c58dbecb62d68
ZininRo <151348599+ZininRo@users.noreply.github.com>
2024-03-19 00:09:08 +0300

    ADS-3 Зинин Роиан 23ИСТ4-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count = 0;
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            count++;
+            int idx = mid - 1;
+            while (idx >= 0 && arr[idx] == value) {
+                count++;
+                idx--;
+            }
+            idx = mid + 1;
+            while (idx < size && arr[idx] == value) {
+                count++;
+                idx++;
+            }
+            break;
+        } else if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return count;
 }
